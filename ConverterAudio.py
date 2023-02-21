d7679a1fbaeffd05a12ddc1271038b7e1894e8d4
Dejdesboisjolis <jeremyb.lebon@gmail.com>
2023-02-21 18:15:35 +0100

    Class

--- ConverterAudio.py
+++ ConverterAudio.py
@@ -3,36 +3,38 @@ import pyttsx3
 import argparse
 
 
-speak = pyttsx3.init()
-
-argParser = argparse.ArgumentParser(
-    exit_on_error=True, description="Welcome to the ConverterAudio, let's choose an operation")
-
-argParser.add_argument(
-    "-f", "--file",
-    nargs='*',
-    help="The fileName you want to select"
-)
-args = argParser.parse_args()
-
-print(args.file)
-
-def read_pdf(args : list):
+class AudioConverter:
+    args = list()
     print(args)
-    for pdf in args :
-        try: 
-            with open(pdf, 'rb') as f:
-                pdf_read = PdfFileReader(f)
-                for x in range (pdf_read.getNumPages()):
-                    page = pdf_read.getPage(x)
-                    page_content = page.extractText()
-                    #print(page_content)
-                    speak.save_to_file(page_content, pdf+'.mp3')
-                    print(f" Le fichier contient {pdf_read.getNumPages()} pages")
-                    speak.say(f" Le fichier contient {pdf_read.getNumPages()} pages")
-                    speak.runAndWait()
-                    speak.stop()
-        except IOError:
-            print (f"Could not read file : {pdf}")
+    def __init__(self):
+        argParser = argparse.ArgumentParser(
+            exit_on_error=True, description="Welcome to the ConverterAudio, let's choose an operation")
 
-read_pdf(args.file)
+        argParser.add_argument(
+            "-f", "--file",
+            nargs='*',
+            help="The fileName you want to select")
+        AudioConverter.args = argParser.parse_args()
+        print(AudioConverter.args.file)
+
+    def convert_to_pdf(self):
+        print(AudioConverter.args.file)
+        speak = pyttsx3.init()
+        for pdf in AudioConverter.args.file :
+            try: 
+                with open(pdf, 'rb') as f:
+                    pdf_read = PdfFileReader(f)
+                    for x in range (pdf_read.getNumPages()):
+                        page = pdf_read.getPage(x)
+                        page_content = page.extractText()
+                        #print(page_content)
+                        speak.save_to_file(page_content, pdf+'.mp3')
+                        print(f" Le fichier contient {pdf_read.getNumPages()} pages")
+                        speak.say(f" Le fichier contient {pdf_read.getNumPages()} pages")
+                        speak.runAndWait()
+                        speak.stop()
+            except IOError:
+                print (f"Could not read file : {pdf}")
+
+audioConv = AudioConverter()
+audioConv.convert_to_pdf()
